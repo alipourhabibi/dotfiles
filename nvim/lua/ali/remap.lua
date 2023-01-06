d5ef3401885d7ecff9b0d59566f2473658b2f2e7
ali <ali.pourhabibi1379@gmail.com>
2023-01-06 10:15:26 +0330

    change some i3 workspace and add GoToReferences lsp nvim

--- nvim/lua/ali/remap.lua
+++ nvim/lua/ali/remap.lua
@@ -9,6 +9,7 @@ nnoremap("th", "<cmd>tabfirst<CR>")
 nnoremap("tl", "<cmd>tablast<CR>")
 
 nnoremap("<C-h>", "<cmd>lua vim.lsp.buf.definition()<CR>")
+nnoremap("<C-H>", "<cmd>lua vim.lsp.buf.references()<CR>")
 
 nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>F", "<cmd>Telescope live_grep<CR>")
