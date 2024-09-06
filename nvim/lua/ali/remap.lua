a084ebc54d0b75c860a7784eed7f818f573c0a15
Ali Pourhabibi <ali.pourhabibi1379@gmail.com>
2024-09-06 13:55:00 +0330

    feat: add debugger remap neovim

--- nvim/lua/ali/remap.lua
+++ nvim/lua/ali/remap.lua
@@ -17,3 +17,28 @@ nnoremap("<leader>F", "<cmd>Telescope live_grep<CR>")
 nnoremap("<leader>y", '"+y')
 nnoremap("<leader>p", '"+p')
 nnoremap('<space>e', '<cmd>lua vim.diagnostic.open_float(0, {scope="line"})<CR>')
+
+-- Function to set key mappings for a given mode
+local function map(mode, key, result, opts)
+  opts = opts or {}
+  vim.api.nvim_set_keymap(mode, key, result, opts)
+end
+
+-- Define key mappings for dap
+map('n', '<leader>db', '<cmd>DapToggleBreakpoint<CR>', { noremap = true, silent = true, desc = 'Add breakpoint at line' })
+map('n', '<leader>dus', [[:lua require('dap.ui.widgets').sidebar(require('dap.ui.widgets').scopes).open()<CR>]], { noremap = true, silent = true, desc = 'Open debugging sidebar' })
+
+-- Define key mappings for dap-go
+map('n', '<leader>dgt', [[:lua require('dap-go').debug_test()<CR>]], { noremap = true, silent = true, desc = 'Debug go test' })
+map('n', '<leader>dgl', [[:lua require('dap-go').debug_last()<CR>]], { noremap = true, silent = true, desc = 'Debug last go test' })
+
+-- Define key mappings for gopher
+map('n', '<leader>gsj', '<cmd>GoTagAdd json<CR>', { noremap = true, silent = true, desc = 'Add json struct tags' })
+map('n', '<leader>gsy', '<cmd>GoTagAdd yaml<CR>', { noremap = true, silent = true, desc = 'Add yaml struct tags' })
+
+-- Key mappings for easier use of DAP (optional but recommended)
+vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
+vim.api.nvim_set_keymap('n', '<F5>', ':lua require"dap".continue()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F6>', ':lua require"dap".step_over()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F7>', ':lua require"dap".step_into()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F8>', ':lua require"dap".step_out()<CR>', {noremap = true, silent = true})
